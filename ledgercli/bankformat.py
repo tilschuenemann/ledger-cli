c176ce6471cbec3dfa6b6ad4bd9271ea7d29a753
schuenemann <til.schuenemann@mailbox.org>
2022-10-13 10:17:26 +0200

    feat: check bank_format internally

--- ledgercli/bankformat.py
+++ ledgercli/bankformat.py
@@ -5,16 +5,21 @@ import pathlib
 
 
 class BankFormat:
+
+    bank_formats = ["dkb"]
+
     @staticmethod
-    def list_bank_format() -> None:
+    def list_bank_formats() -> None:
         print("supported bank_formats:")
-        for bank_format in ["dkb"]:
+        for bank_format in BankFormat().bank_formats:
             print(bank_format)
 
     @staticmethod
     def get_transactions(bank_format: str, export_path: pathlib.Path) -> pd.DataFrame:
+        if bank_format not in BankFormat().bank_formats:
+            exit("The bank_format you provided is not supported (yet)!")
         if bank_format == "dkb":
-            df = pd.read_csv(
+            tmp = pd.read_csv(
                 export_path,
                 sep=";",
                 decimal=",",
@@ -24,11 +29,13 @@ class BankFormat:
                 encoding="latin1",
                 skiprows=6,
             )
-            df = df.iloc[:, [0, 3, 7]].copy()
+            df = tmp.iloc[:, [0, 3, 7]].copy()
         return df
 
     @staticmethod
     def get_end_balance(bank_format: str, export_path: pathlib.Path) -> pd.DataFrame:
+        if bank_format not in BankFormat().bank_formats:
+            exit("The bank_format you provided is not supported (yet)!")
         if bank_format == "dkb":
             header = pd.read_csv(
                 export_path,
