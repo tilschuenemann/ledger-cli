298441646964a22a2037eb9f32a9b0bdb6607034
tilschuenemann <til.schuenemann@mailbox.org>
2023-02-24 10:41:29 +0100

    tests: group ledger tests

--- tests/test_ledger.py
+++ tests/test_ledger.py
@@ -222,3 +222,50 @@ def test_write(output_dir: Path, export_path: Path) -> None:
     ]:
         tmp = output_dir / item
         assert tmp.exists()
+
+
+def test_update(output_dir: Path, export_path: Path) -> None:
+    """Tests for updating existing files."""
+    # setup existing files
+    ledger = Ledger(output_dir, bank="dkb")
+    ledger.update(export_path)
+    ledger.write()
+
+    ledger = Ledger(output_dir, bank="dkb")
+
+    assert ledger.tx.shape == (1, 15)
+    assert ledger.mapping.empty is False
+    assert ledger.metadata.empty is False
+
+    # adding more transactions
+    ledger.update(export_path)
+    ledger.update(export_path)
+    assert ledger.tx.shape == (3, 15)
+
+    # update without new export
+    ledger.update()
+    assert ledger.tx.shape == (3, 15)
+
+
+def test_metadata(output_dir: Path, export_path: Path) -> None:
+    """Tests for falling back to existing metadata."""
+    # init without bank
+    with pytest.raises(Exception) as exc_info:
+        ledger = Ledger(output_dir, bank=None)
+    assert (
+        str(exc_info.value)
+        == "Please supply a valid BANK! Couldn't read BANK from metadata."
+    )
+
+    # setup existing files
+    ledger = Ledger(output_dir, bank="dkb")
+    ledger.update(export_path)
+    ledger.write()
+
+    assert ledger.metadata.empty is False
+    assert ledger.metadata["bank"].iloc[0] == "dkb"
+    assert (output_dir / "metadata.csv").exists()
+
+    # init with bank
+    ledger = Ledger(output_dir, bank=None)
+    assert ledger.metadata["bank"].iloc[0] == "dkb"
