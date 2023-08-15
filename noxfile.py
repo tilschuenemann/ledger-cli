8d501de69a8a528da53bca0888075c839e43e546
Til Schünemann <til.schuenemann@mailbox.org>
2023-08-15 13:21:02 +0200

    fix: adjust python version in nox

--- noxfile.py
+++ noxfile.py
@@ -23,7 +23,7 @@ except ImportError:
 
 
 package = "ledgercli"
-python_versions = ["3.10"]
+python_versions = ["3.11"]
 nox.needs_version = ">= 2021.6.6"
 nox.options.sessions = (
     "pre-commit",
@@ -52,8 +52,7 @@ def activate_virtualenv_in_precommit_hooks(session: Session) -> None:
     # quoting rules for Python and bash, but strip the outermost quotes so we
     # can detect paths within the bindir, like <bindir>/python.
     bindirs = [
-        bindir[1:-1] if bindir[0] in "'\"" else bindir
-        for bindir in (repr(session.bin), shlex.quote(session.bin))
+        bindir[1:-1] if bindir[0] in "'\"" else bindir for bindir in (repr(session.bin), shlex.quote(session.bin))
     ]
 
     virtualenv = session.env.get("VIRTUAL_ENV")
@@ -95,10 +94,7 @@ def activate_virtualenv_in_precommit_hooks(session: Session) -> None:
 
         text = hook.read_text()
 
-        if not any(
-            Path("A") == Path("a") and bindir.lower() in text.lower() or bindir in text
-            for bindir in bindirs
-        ):
+        if not any(Path("A") == Path("a") and bindir.lower() in text.lower() or bindir in text for bindir in bindirs):
             continue
 
         lines = text.splitlines()
@@ -227,9 +223,7 @@ def docs(session: Session) -> None:
     """Build and serve the documentation with live reloading on file changes."""
     args = session.posargs or ["--open-browser", "docs", "docs/_build"]
     session.install(".")
-    session.install(
-        "sphinx", "sphinx-autobuild", "sphinx-click", "sphinx_rtd_theme", "myst-parser"
-    )
+    session.install("sphinx", "sphinx-autobuild", "sphinx-click", "sphinx_rtd_theme", "myst-parser")
 
     build_dir = Path("docs", "_build")
     if build_dir.exists():
